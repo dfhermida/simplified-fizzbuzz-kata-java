2354700658daebd500c50e551d77fa1114bd649b
David Fernández Hermida <dfernandez@gradiant.org>
2022-02-23 17:50:15 +0100

    Added one test that fails
    The test check if FizzBuzz.translate() return 1 when 1 is passed.
    
    The test fails because no method translate exists in FizzBuzz class.

--- src/test/fizzbuzz/FizzBuzzTest.java
+++ src/test/fizzbuzz/FizzBuzzTest.java
@@ -10,4 +10,10 @@ public class FizzBuzzTest {
 	public void get_name_should_return_fizz_buzz_game_name() {
 		assertEquals("FizzBuzz Game", FizzBuzz.getName());
 	}
+
+	@Test
+	public void translate_should_return_string_1_if_number_1_is_passed() {
+		assertEquals("1", FizzBuzz.translate(1));
+	}
+
 }
