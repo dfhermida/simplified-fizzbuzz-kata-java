e323050776a1065d6d2e673f2b0d0499e5d00a98
David Fernández Hermida <dfernandez@gradiant.org>
2022-02-23 18:04:30 +0100

    Code now returns Fizz when 3 is passed

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,9 @@ public class FizzBuzz {
     }
 
     public static String translate(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
         return Integer.toString(number);
     }
 }
