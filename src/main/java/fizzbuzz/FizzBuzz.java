1542e7721354b735580b65a0e6e812cf9864cf52
David Fernández Hermida <dfernandez@gradiant.org>
2022-02-23 17:51:53 +0100

    Added minimum code that makes the test pass.
    Created method translate() and return the expected value.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,4 +4,8 @@ public class FizzBuzz {
     public static String getName() {
         return "FizzBuzz Game";
     }
+
+    public static String translate(int number) {
+        return "1";
+    }
 }
